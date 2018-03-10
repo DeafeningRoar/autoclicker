adca9fb215cd77c7e734731ee72d162c03315ce4
DeafeningRoar <finalx7@gmail.com>
2018-03-10 03:27:46 -0300

    Files Ready to compile

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const iohook = require('iohook');
-const spawn = require('child_process').spawn;
+const exec = require('child_process').execFile;
 
 var start = false;
 var child;
@@ -14,7 +14,7 @@ iohook.on('keydown', event => {
     if(event.keycode === 43){
         start = !start
         if(start){
-            child = spawn('node', ['clicker.js']);
+            child = exec('clickLoop');
             child.on('exit', () => start = false);
         } else {
             child.kill();
